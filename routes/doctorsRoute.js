3af4d8efb3695a9efda8f24f6f1e5036e37f03e9
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-04-05 18:38:00 +0530

    doctor

---
+++ routes/doctorsRoute.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const Doctor = require("../models/doctorModel");
+const authMiddleware = require("../middlewares/authMiddleware");
+const Appointment = require("../models/appointmentModel");
+const User = require("../models/userModel");
+
+router.post("/get-doctor-info-by-user-id", authMiddleware, async (req, res) => {
+  try {
+    const doctor = await Doctor.findOne({ userId: req.body.userId });
+    res.status(200).send({
+      success: true,
+      message: "Doctor info fetched successfully",
+      data: doctor,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "Error getting doctor info", success: false, error });
+  }
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
