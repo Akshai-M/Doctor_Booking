452ff0521716a5c1459321ba0a6dd9318a469da5
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-04-05 19:42:20 +0530

    updated

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
 const port = process.env.PORT || 5000;
