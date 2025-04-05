a3298a9dba91d90d399fde3f902b48b5baded2ad
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-04-05 18:39:01 +0530

    admin

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -22,7 +22,23 @@ router.get("/get-all-doctors", authMiddleware, async (req, res) => {
   }
 });
 
-
+router.get("/get-all-users", authMiddleware, async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send({
+      message: "Users fetched successfully",
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Error applying doctor account",
+      success: false,
+      error,
+    });
+  }
+});
 
 
 
