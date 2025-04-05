fad1022ecd0563cae95872634f60913ba7461f59
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-04-05 18:36:34 +0530

    user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,6 +8,43 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const Appointment = require("../models/appointmentModel");
 const moment = require("moment");
 
+router.post("/register", async (req, res) => {
+  try {
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res
+        .status(200)
+        .send({ message: "User already exists", success: false });
+    }
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
+    const newuser = new User(req.body);
+    await newuser.save();
+    res
+      .status(200)
+      .send({ message: "User created successfully", success: true });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Error creating user", success: false, error });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 router.get("/get-appointments-by-user-id", authMiddleware, async (req, res) => {
